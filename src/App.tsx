c797bd341e2be1b5096a10dfdc322d7fc111a17c
alexamaingard <maingard.alexa@gmail.com>
2025-07-17 15:28:04 +0200

    Add content to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,46 @@
-// import { motion } from 'framer-motion';
+// import { motion, Transition } from 'framer-motion';
 
+import { AboutSection } from './components/AboutSection';
+// import { ExperienceSection } from './components/ExperienceSection';
+import { Footer } from './components/Footer';
 import { Header } from './components/Header';
+import { IntroPage } from './components/IntroPage';
+import { SocialMedia } from './components/SocialMedia';
 import { GlobalStyles } from './styles/GlobalStyles';
+import { pageMarginStyle } from './styles/shared';
 
 export default function App() {
+  // const motionTransitionProps: Transition<unknown> | undefined = {
+  //   duration: 1.5,
+  //   delay: 0.3,
+  //   ease: "easeInOut",
+  // };
+
   return (
     <>
       <GlobalStyles />
       <Header />
-      <main>
-        {/* add content here */}
+      <main css={pageMarginStyle}>
+        <SocialMedia />
+        <IntroPage />
+        {/* <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{
+            duration: 1.5,
+            delay: 0.3,
+            ease: "easeInOut",
+          }}
+          viewport={{ once: true }}
+        > */}
+          <AboutSection />
+        {/* </motion.div> */}
+        {/* <ExperienceSection /> */}
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
-
-
-// import { motion } from 'framer-motion';
-
-// import { AboutPage } from './components/AboutPage';
-// import { Contact } from './components/Contact';
-// import { Footer } from './components/Footer';
-// import { Header } from './components/Header';
-// import { Portfolio } from './components/Portfolio';
-// import { ProfilePage } from './components/ProfilePage';
-// import { SocialMedia } from './components/SocialMedia';
-
-// import './styles/App.css';
-// import './styles/reset.css';
 
 // export const App = () => {
 //   return (
@@ -36,16 +48,16 @@ export default function App() {
 //       <Header />
 //       <SocialMedia />
 //       <ProfilePage />
-//       <motion.div
-//         initial={{ opacity: 0 }}
-//         whileInView={{ opacity: 1 }}
-//         transition={{
-//           duration: 1.5,
-//           delay: 0.1,
-//           ease: "easeInOut"
-//         }}
-//         viewport={{ once: true }}
-//       >
+      // <motion.div
+      //   initial={{ opacity: 0 }}
+      //   whileInView={{ opacity: 1 }}
+      //   transition={{
+      //     duration: 1.5,
+      //     delay: 0.1,
+      //     ease: "easeInOut"
+      //   }}
+      //   viewport={{ once: true }}
+      // >
 //         <AboutPage />
 //       </motion.div>
 //       <motion.div
