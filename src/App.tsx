fc207199f915e5015bdb3fd8e248627b07d6387f
alexamaingard <maingard.alexa@gmail.com>
2025-08-13 15:43:07 +0200

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { AboutSection } from './components/AboutSection';
 import { ContactSection } from './components/Contact';
